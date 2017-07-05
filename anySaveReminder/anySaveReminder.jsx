2a082fcb10681f7917e361d689abf001f30ba5b9
Unknown <lennert@any.green>
2017-07-05 14:01:46 +0200

    re organized

---
+++ anySaveReminder/anySaveReminder.jsx
@@ -0,0 +1,17 @@
+//alert(app.project.activeItem.selectedLayers[0].property("Effects").property("Corner Pin").property("Upper Left").value);
+try{
+
+  function doSave(){
+    if (app.project.file == null && app.project.numItems > 0) {
+      alert("You have not saved your project yet! \r Let's do it now ;)");
+      app.project.saveWithDialog();
+    } else {
+      //all good, the project has been saved
+    }
+  };
+
+  app.scheduleTask("doSave()", 240000, true);
+
+}catch(err){
+  alert("Error in line: " + err.line + "\n" + err.toString());
+}
