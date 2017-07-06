80468b37dfda31bf26eb6fc426be074517a93feb
nhb <lennert@any.green>
2017-07-06 13:10:52 +0200

    Updated ScriptTester with additional options: The user now has the option of a button to choose the file he wants to open. And the edittext field now automatically converts spaces.

--- ScriptTester/ScriptTester.jsx
+++ ScriptTester/ScriptTester.jsx
@@ -1,12 +1,12 @@
 {
 function myScript(thisObj){
   function myScript_buildUI(thisObject){
-    var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette","My Window Name", undefined, {resizeable:true});
+    var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette","ScriptTester", undefined, {resizeable:true});
 
     res = "group{orientation:'column', alignment:['fill','top'], alignChildren:['fill','fill'],\
             groupOne: Group{orientation:'column',\
-              myStaticText: StaticText{text:''},\
-              myInput: EditText{text:'/Users/nhb/github/ae_tools/', preferredSize:[400,-1]},\
+              myButtonChoose: Button{text:'Choose'},\
+              myInput: EditText{text:'[enter your path here or use the button above]', preferredSize:[400,-1]},\
               myButton: Button{text:'Run'},\
             },\
           }";
@@ -25,13 +25,27 @@ function myScript(thisObj){
     /////////////////
     //THE SCRIPTING//
     /////////////////
+    myPanel.grp.groupOne.myButtonChoose.onClick = function () {
+      try {
+        var theFile = File.openDialog();
+        if (theFile != null) {
+            myPanel.grp.groupOne.myInput.text = theFile.fsName;
+        }
+      } catch (e) {
+        alert("Error in line: " + e.line + "\n" + e.toString());
+      }
+    };
 
     myPanel.grp.groupOne.myButton.onClick = function () {
       try {
-        var scriptFile = File(myPanel.grp.groupOne.myInput.text.toString());
-        var script = '#include' + scriptFile.fullName;
+        var theFile = new File(myPanel.grp.groupOne.myInput.text.toString());
+        if (theFile.exists) {
+          var script = '#include' + myPanel.grp.groupOne.myInput.text.toString().replace(/ /g, '%20');
+          eval(script);
+        } else {
+          alert("File doesn't exist :(");
+        }
 
-        eval(script);
       } catch (e) {
         alert("Error in line: " + e.line + "\n" + e.toString());
       }
