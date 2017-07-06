12fba0a485defb4a7dae500b968eaf160602b61d
Unknown <lennert@any.green>
2017-07-06 18:06:36 +0200

    anyOrigin initial release

--- anyOrigin/anyOrigin.jsx
+++ anyOrigin/anyOrigin.jsx
@@ -1,26 +1,18 @@
 {
 function myScript(thisObj){
   function myScript_buildUI(thisObject){
-    var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette","Loops", undefined, {resizeable:true});
+    var myPanel = (thisObj instanceof Panel) ? thisObj : new Window("palette","anyOrigin v0.1", undefined, {resizeable:true});
 
-    res = "group{orientation:'column', alignment:['fill','top'], alignChildren:['fill','fill'],\
-            groupOne: Group{orientation:'column',\
-              myStaticText: StaticText{text:'anyLoops v0.1'},\
-            },\
-            groupTwo: Group{orientation:'column', alignment:['center','top'],\
-              loopsDropdownInOut: DropDownList{properties:{items:['Loop IN','Loop OUT','Loop IN/OUT']}},\
-              loopsDropdownType: DropDownList{properties:{items:['cycle','continue','pingpong','offset']}},\
-              loopsButton: Button{text:'Apply'},\
-              loopsReplace: Checkbox{text:'replace previous Expression'},\
-            },\
+    res = "group{orientation:'column',\
+              groupOne: Group{orientation:'row',\
+                btnSave: Button{text:'Save', preferredSize:[50,-1]},\
+                btnRevert: Button{text:'Revert', preferredSize:[50,-1]},\
+              },\
+              btnClear: Button{text:'Clear', preferredSize:[50,15]},\
+              staticText: StaticText{text:'', preferredSize:[200,15]},\
           }";
 
     myPanel.grp = myPanel.add(res);
-
-    //Defaults:
-    myPanel.grp.groupTwo.loopsDropdownInOut.selection = 1;
-    myPanel.grp.groupTwo.loopsDropdownType.selection = 0;
-    myPanel.grp.groupTwo.loopsReplace.value = true;
 
     //Setup Panel Sizing
     myPanel.layout.layout(true);
@@ -30,56 +22,140 @@ function myScript(thisObj){
     myPanel.layout.resize();
     myPanel.onResizing = myPanel.onResize = function(){this.layout.resize()};
 
+
     /////////////////
     //THE SCRIPTING//
     /////////////////
 
-    //LOOPS START
-    myPanel.grp.groupTwo.loopsButton.onClick = function () {
-      try {
-        app.beginUndoGroup("Applied Loop");
-        var proj = app.project;
-        var activeItem = proj.activeItem;
-        var selectedLayers = activeItem.selectedLayers;
-        var selectedProps = activeItem.selectedProperties;
+    var btnSave = myPanel.grp.groupOne.btnSave;
+    var btnRevert = myPanel.grp.groupOne.btnRevert;
+    var btnClear = myPanel.grp.btnClear;
 
-        var loopInOut = myPanel.grp.groupTwo.loopsDropdownInOut.selection;
-        var loopType = myPanel.grp.groupTwo.loopsDropdownType.selection;
-        var loopReplace = myPanel.grp.groupTwo.loopsReplace.value;
+    var proj, activeItem, selectedLayers;
 
-        var expression;
-        if (loopInOut.toString() == "Loop IN/OUT") {
-          expression = '//Loop IN/OUT\nif (time < key(1).time) loopIn("'+loopType.toString()+'");\nif (time >= key(1).time && time <= key(numKeys).time) value;\nif (time > key(numKeys).time) loopOut("'+loopType.toString()+'");';
-        }
-        if (loopInOut.toString() == "Loop IN") {
-          expression ='//Loop IN\nloopIn("'+loopType.toString()+'");';
-        }
-        if (loopInOut.toString() == "Loop OUT") {
-          expression ='//Loop OUT\nloopOut("'+loopType.toString()+'");';
-        }
+    function getLayers(){
+      var selectedLayers = app.project.activeItem.selectedLayers;
+      return selectedLayers
+    }
 
-        if (selectedProps == "") {
-          alert("The following Error occured:\nNo property selected")
-        } else {
-          //looping through all selected properties (including groups)
-          for (var i = 0; i < selectedProps.length; i++) {
-            var currentProp = selectedProps[i]; //defining currently selected property
-            if(currentProp.numKeys!=undefined&&currentProp.canSetExpression==true){ //checking if property has keyframes do see if its a group
-              if (loopReplace) {
-                currentProp.expression = expression;
-              } else {
-                currentProp.expression = currentProp.expression+"\n"+expression;
-              }
-            }
+    function setMarkerWithOrigin(layer,origin){
+      var originString = getTransforms(layer);
+      var mv = new MarkerValue("anyOrigin");
+      var parms = new Object;
+      parms.transforms = originString;
+      mv.setParameters(parms);
+      layer.property("Marker").setValueAtTime(0, mv);
+    }
+
+    function getTransforms(layer){
+      var anchorPoint = layer.transform.anchorPoint.value;
+      var position = layer.transform.position.value;
+      var scale = layer.transform.scale.value;
+      var rotation = layer.transform.rotation.value;
+      var opacity = layer.transform.opacity.value;
+
+      var transformArray = [];
+      transformArray.push(anchorPoint.toString());
+      transformArray.push(position.toString());
+      transformArray.push(scale.toString());
+      transformArray.push(rotation.toString());
+      transformArray.push(opacity.toString());
+
+      var transformString = transformArray.join(";");
+
+      return transformString
+    }
+
+    function removeMarkers(){
+      var layer = getLayers();
+      for (var i = 0; i < layer.length; i++) {
+        var length = layer[i].property("Marker").numKeys;
+        var originMarker = [];
+        for (var l = 1; l < length+1; l++) {
+          var comment = layer[i].property("Marker").keyValue(l).comment;
+          if (comment == "anyOrigin") {
+            originMarker.push(l);
           }
         }
-        app.endUndoGroup();
-      } catch (e) {
-        alert("Error in line: " + e.line + "\n" + e.toString());
+        if (originMarker.length > 0) {
+          for (var r = originMarker.length-1; r >= 0; r=r-1) {
+            layer[i].property("Marker").removeKey(originMarker[r]);
+          }
+        }
       }
-    };
-    //LOOPS END
+    }
 
+    function revertOrigin(){
+      var layer = getLayers();
+      for (var i = 0; i < layer.length; i++) {
+        var length = layer[i].property("Marker").numKeys;
+        var originMarker = [];
+        for (var l = 1; l < length+1; l++) {
+          var comment = layer[i].property("Marker").keyValue(l).comment;
+          if (comment == "anyOrigin") {
+            originMarker.push(l);
+          }
+        }
+        if (originMarker.length > 1) {
+          alert("There are multiple anyOrigin markers.\nOnly the first one will be used to revert to origin.");
+        }
+        var originString = layer[i].property("Marker").keyValue(originMarker[0]).getParameters().transforms;
+        var originObj = getOriginObj(originString);
+        applyOrigin(layer[i],originObj);
+      }
+    }
+
+    function getOriginObj(string){
+      var originArray = string.split(";");
+      originObj = {};
+      originObj.anchorPoint = originArray[0].split(",");
+      originObj.position = originArray[1].split(",");
+      originObj.scale = originArray[2].split(",");
+      originObj.rotation = originArray[3];
+      originObj.opacity = originArray[4];
+      return originObj
+    };
+
+    function applyOrigin(layer,originObj){
+      removeAllKeys(layer.transform.anchorPoint);
+      removeAllKeys(layer.transform.position);
+      removeAllKeys(layer.transform.scale);
+      removeAllKeys(layer.transform.rotation);
+      removeAllKeys(layer.transform.opacity);
+
+      layer.transform.anchorPoint.setValue(originObj.anchorPoint);
+      layer.transform.position.setValue(originObj.position);
+      layer.transform.scale.setValue(originObj.scale);
+      layer.transform.rotation.setValue(originObj.rotation);
+      layer.transform.opacity.setValue(originObj.opacity);
+    }
+
+    function removeAllKeys(property){
+      for (var i = property.numKeys; i >= 1 ; i=i-1) {
+        property.removeKey(i);
+      }
+    }
+
+    btnSave.onClick = function () {
+      app.beginUndoGroup("anyOrigin Save");
+        var layer = getLayers();
+        for (var i = 0; i < layer.length; i++) {
+          setMarkerWithOrigin(layer[i]);
+        }
+      app.endUndoGroup();
+    }
+
+    btnClear.onClick = function () {
+      app.beginUndoGroup("anyOrigin Clear");
+      removeMarkers();
+      app.endUndoGroup();
+    }
+
+    btnRevert.onClick = function () {
+      app.beginUndoGroup("anyOrigin Revert");
+      revertOrigin();
+      app.endUndoGroup();
+    }
     ///////////
     //THE END//
     ///////////
